1adb421e8e140bb7b30ec8a83da8d42a4af548c5
koki murakami <koki.m3626@gmail.com>
2020-05-22 03:31:47 +0000

    second

--- practice.php
+++ practice.php
@@ -5,7 +5,7 @@ function double($i){
     return $i*2;
 }
 
-echo double(3);
+echo double(3)."\n";
 
 //2.$a と $b を仮引数に持ち、　$a と $b　を足した結果を返す関数を作成してください。
 
@@ -13,7 +13,7 @@ function plusab($a,$b){
     return $a+$b;
 }
 
-echo plusab(2,23);
+echo plusab(2,23)."\n";
 
 // 3.$arr という配列の仮引数を持ち、数値が入った配列array(1, 3, 5 ,7, 9) を渡すとその要素をすべてかけた結果を返す関数を作成してください。
 function product($arr){
@@ -24,7 +24,7 @@ function product($arr){
     return $result;
 }
 
-echo product(array(1,3,5,7,9));
+echo product(array(1,3,5,7,9))."\n";
 
 //4.【応用】　次のプログラムは、配列の中で一番大きい値を返す max_array という関数を実装しようとしています。途中の部分を完成させてください。
 function max_array($arr){
@@ -37,4 +37,39 @@ function max_array($arr){
  }
  return $max_number;
  }
-echo max_array(array(1,23,5,2,8));
+echo max_array(array(1,23,5,2,8))."\n";
+
+//5.次のビルトイン関数の用途、使い方を調べて実際に使ってみてください。
+
+$str ="<h1>h1タグ</h1>"."<p>pタグ</p>"."\n";
+
+echo strip_tags($str);
+
+echo strip_tags($str,"<p>");
+
+$fruits1 =['banana','apple','kiwi'];
+
+array_push($fruits1,'melon','strawberry');
+
+print_r($fruits1);
+
+$fruits2 =['pineapple','cherry','orange'];
+$fruits3 =['papaya','anzu'];
+
+$fruits_merge =array_merge($fruits1,$fruits2,$fruits3);
+
+var_dump($fruits_merge);
+
+date_default_timezone_set('Asia/Tokyo');
+print date("Y/m/d G:i:s")."\n";
+
+echo time()."\n";
+echo mktime(2,23,4,12,24,2020)."\n";
+
+
+$yearAfterOne = time() + (365*24*60*60);
+echo $yearAfterOne."\n";
+print date("Y/m/d G:i:s",$yearAfterOne)."\n";
+
+
+?>
